469ec668e6adb0598ca3d9bfee2f3d623f0ef925
trakjohnson <trakjohnsonweb@gmail.com>
2017-05-13 16:54:22 +0200

    cross-OS path detection

--- blender_script.py
+++ blender_script.py
@@ -3,16 +3,23 @@ This is NOT for running standalone. You must use it as a blender script
 """
 import sys
 import bpy
-# required for import TODO that only works for windows x(
-sys.path.append('\\'.join(bpy.data.filepath.split('\\')[:-2]))
+# required for import
+token = '/' if '/' == bpy.data.filepath[0] else '\\'  # cross-OS
+sys.path.append(
+    token.join(bpy.data.filepath.split(token)[:-2])
+)
 from electromagnetism import ChargeSystem, Charge, process_coordinates
 
 
-def electron_animation_from_coords(name, coords, initial_location=(0, 0, 0), frames_per_keyframe=7):
-    """Animate an electron from a list of coordinates"""
-    electron_mesh = bpy.ops.mesh.primitive_uv_sphere_add(
-        location=initial_location
-    )
+def electron_animation_from_coords(name: str, coords: list, initial_location=(0, 0, 0), frames_per_keyframe=1) -> None:
+    """
+    Animate an electron from a list of coordinates
+    :param name: name of the mesh
+    :param coords: animation coords
+    :param initial_location: location upon mesh creation
+    :param frames_per_keyframe: # of frames after each keyframe
+    """
+    electron_mesh = bpy.ops.mesh.primitive_uv_sphere_add(location=initial_location)
     electron_object = bpy.context.object
     electron_object.name = name
 
@@ -22,9 +29,10 @@ def electron_animation_from_coords(name, coords, initial_location=(0, 0, 0), fra
 
 
 v = ChargeSystem(3)
-c1 = Charge(-5 * 10 ** -5, v, position=[0, 0, 0])
-c2 = Charge(5 * 10 ** -5, v, position=[10, 10, 10])
-anim = process_coordinates(v, 60)
+c1 = Charge(-5 * 10 ** -5, v, position=[-10, -10, 0])
+c2 = Charge(5 * 10 ** -5, v, position=[0, 10, 0])
+c3 = Charge(3 * 10 ** -5, v, position=[10, 0, 0])
+anim = process_coordinates(v, 1200)
 
 for ind, charge_coords in enumerate(anim):
-    electron_animation_from_coords('electron{}'.format(ind), charge_coords, frames_per_keyframe=4)
+    electron_animation_from_coords('electron{}'.format(ind), charge_coords, initial_location=charge_coords[0])
